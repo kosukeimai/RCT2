b64f6a430eede3c14f87958d3c866d19d3b34f83
Karissa Huang <khuang@karissas-mbp-2.attlocal.net>
2020-09-13 20:56:27 -0400

    add documented data set and S3 method

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Replication Data for: Causal Inference with Interference and Noncompliance in Two-Stage Randomized Experiments.
+#'
+#' @source \url{https://doi.org/10.7910/DVN/N7D9LS}
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{id}{The id for the village.}
+#'  \item{DistrictId}{The id for the district.}
+#'  \item{Z}{The treatment status for the individual.}
+#'  \item{A}{The treatment assignment mechanism.}
+#'  \item{D}{Whether or not the individual enrolled.}
+#'  \item{Y}{The hospital expenditure.}
+#'  \item{X}{Enumeration of the patients.}
+#' }
+
+"india"
